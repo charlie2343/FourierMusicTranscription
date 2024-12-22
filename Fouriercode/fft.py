7901ca13cea77f7026b359359bba61842b02ec30
charlie2343 <117702726+charlie2343@users.noreply.github.com>
2024-12-22 14:39:28 -0800

    Added clef and signature detection

--- Fouriercode/fft.py
+++ Fouriercode/fft.py
@@ -2,8 +2,8 @@ import cmath
 import time
 from scipy.io.wavfile import read
 
-#audio = [0,1,2,3,4,5,6,7]
-SAMPLING_RATE, audio = read("mhll.wav")
+audio = [0,1,2,3,4,5,6,7]
+#SAMPLING_RATE, audio = read("mhll.wav")
 
 def fft(): 
     split_data = []
@@ -37,29 +37,38 @@ def DFT(window):
     return DFT_result
 
 def FFTtest(window): 
-    first = window[0]
-    second = window[1]
-    DFT_result = {}
+    # evenSamples
+    # for i in range(0, len(window)): 
+    #     if i % 2 ==0: 
+    #         evenSamples = window[i] 
+    #     oddSamples = window[1]
+    FFT_result = {}
+    even = 0
+    odd = 0
     # Loop through frequencies up to Nyquist
     for freq in range(4):
         # Initialize the coefficient for the frequency
-        coefficient = 0
+        even =0
+        odd = 0
         for sample in range(4): 
             # Use cmath.exp for complex exponential
-            coefficient += first[sample] * cmath.exp(-2j * cmath.pi * freq / 4 * sample)
-        DFT_result[freq] = coefficient
-        DFT_result[freq+4] = coefficient
-    for freq in range(4): 
-        for sample in range(4): 
-            # Use cmath.exp for complex exponential
-            coefficient += second[sample] * cmath.exp(-2j * cmath.pi * freq / 4 * sample)
-        DFT_result[freq] += coefficient
-        DFT_result[freq+4] -= coefficient
-    return DFT_result
+            even += window[sample*2] * cmath.exp(-2j * cmath.pi * freq / 4 * sample)
+            odd += window[sample*2 +1] * cmath.exp(-2j * cmath.pi * freq / 4 * sample)
+        
+        FFT_result[freq] = even + cmath.exp(-1j * cmath.pi * freq/4) * odd
+        FFT_result[freq+4] = even + odd * cmath.exp(-1j * cmath.pi * (freq+4)/4)
+    
+    # for freq in range(4): 
+    #     for sample in range(4): 
+    #         # Use cmath.exp for complex exponential
+    #         coefficient += second[sample] * cmath.exp(-2j * cmath.pi * freq / 4 * sample)
+    #     DFT_result[freq] += coefficient
+    #     DFT_result[freq+4] -= coefficient
+    return FFT_result
 
 def pairFreq(): 
     firsthalf = audio[slice(0,len(audio))/2]
     secondhalf = sdf
 
 print(DFT(audio))
-
+print(FFTtest(audio))
